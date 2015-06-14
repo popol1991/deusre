95bf2781f15e7a067a92c598c3cc58f965248fd3
Kyle Gao <kyle.ygao@gmail.com>
2015-06-13 22:58:26 -0400

    add prel generating tool

---
+++ evaluation/genPrel.py
@@ -0,0 +1,25 @@
+"""This script generate TREC-format prel file from local judge index
+"""
+
+import json
+from elasticsearch import Elasticsearch
+
+if __name__ == "__main__":
+    es = Elasticsearch()
+
+    body = {
+        "query" : {
+            "match_all": {}
+        },
+        "size" : 100
+    }
+
+    res = es.search(index="judge", doc_type="judge", body=body)
+    hits = res['hits']['hits']
+
+    for qid in xrange(len(hits)):
+        judge = json.loads(hits[qid]['_source']['judge'])
+        prel = judge['prel']
+
+        for docid, rel in prel.items():
+            print "{0} Q0 {1} {2}".format(qid, docid, rel)
