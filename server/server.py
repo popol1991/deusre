9095ea37c01bd20293f75d24a7480a84c65498f6
Yingkai Gao <yingkaig@boston-cluster.lti.cs.cmu.edu>
2015-10-13 18:31:19 -0400

    update logging

--- server/server.py
+++ server/server.py
@@ -1,4 +1,6 @@
 import sys
+import time
+import datetime
 import json
 import cookies
 from es import ES
@@ -25,6 +27,11 @@ es = None
 config = json.load(open("config.json"))
 es = ES(config['es_server'])
 
+def getTimestamp():
+    ts = time.time()
+    stamp = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+    return stamp
+
 @app.route("/deusre/arxiv/subjects.json")
 def subjects():
     with open("./static/data/subjects.json") as fin:
@@ -34,7 +41,7 @@ def subjects():
 @app.route("/deusre/arxiv/doc/<path:path>")
 def route(path):
     userid, docid = path.split(":")
-    logger.info("User from {0} with id {1} clicked doc with id {2}".format(request.remote_addr, userid, docid))
+    logger.info("[{0}]\tUser from {1} with id {2} clicked doc with id {3}".format(getTimestamp(), request.remote_addr, userid, docid))
     return redirect("http://arxiv.org/abs/{0}".format(docid))
 
 @app.route("/deusre/arxiv/submit")
@@ -45,7 +52,7 @@ def arxiv():
     params = request.args
     if len(params) == 0:
         return render_template('arxiv.html', len=0, hits=[], query="", params={}, dataset="elsevier", pages=0, visible=0, page=0, userid=userid)
-    logger.info("_QUERY: user={0} ip={1} query={2}".format(userid, request.remote_addr, json.dumps(params)))
+    logger.info("[{0}]\t_QUERY: user={1} ip={2} query={3}".format(getTimestamp(), userid, request.remote_addr, json.dumps(params)))
     # get domain
     domainlist = [params[key] for key in params if key.startswith('subdomain')]
     if len(domainlist) == 0 or domainlist[0] == 'all':
@@ -72,12 +79,13 @@ def arxiv():
             "from": page * DEFAULT_SIZE,
             "size": DEFAULT_SIZE
         }
+    print body
     res = es.search(ARXIV_INDEX, body=body)
     numRes = res['hits']['total']
     res = ESResponse(res)
-    logger.info("Reranking...")
+    logger.info("[{0}]\tReranking...".format(getTimestamp()))
     res = res.rerank(params)
-    logger.info("Rendering...")
+    logger.info("[{0}]\tRendering...".format(getTimestamp()))
     for hit in res:
         if len(hit['subdomains']) != 0:
             hit['subject'] = ", ".join(hit['subdomains'])
